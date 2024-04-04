8f4e9c75ec8ea23e78b56f4a5790fc874a2af2e8
Vince L <viwliu@ucsc.edu>
2024-04-04 14:14:52 -0700

    finished

--- CSE160/Assignment 1/asg0.js
+++ CSE160/Assignment 1/asg0.js
@@ -1,116 +1,90 @@
 function main() {
-    // Retrieve <canvas> element
-    canvas = document.getElementById('asg0');
-    ctx = canvas.getContext('2d');
-    // black canvas
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, 400, 400);
+  canvas = document.getElementById('asg0');
+  board = canvas.getContext('2d');
+  board.fillStyle = 'black';
+  board.fillRect(0, 0, 400, 400);
 }
 
 function drawVector(v, color) {
-    ctx.beginPath();
-    ctx.moveTo(200, 200);
-    ctx.lineTo(200 + v.elements[0] * 20, 200 - v.elements[1] * 20, 20 * v.elements[2]);
-    ctx.strokeStyle = color;
-    ctx.stroke();
+  board.beginPath();
+  board.moveTo(200, 200);
+  board.lineTo(200+v.elements[0]*20, 200-v.elements[1]*20, 20*v.elements[2]);
+  board.strokeStyle = color;
+  board.stroke();
 }
 
 function handleDrawEvent() {
-    // Clear Canvas
-    ctx.clearRect(0, 0, 400, 400);
-
-    // Black blackground
-    ctx.fillStyle = 'black'; // Set color to black
-    ctx.fillRect(0, 0, 400, 400);
-
-    //read input
-    let x = document.getElementById('v1x').value;
-    let y = document.getElementById('v1y').value;
-    let x2 = document.getElementById('v2x').value;
-    let y2 = document.getElementById('v2y').value;
-
-    // Draw new lines
-    let v1 = new Vector3([x, y, 0.0]);
-    drawVector(v1, "red");
-    let v2 = new Vector3([x2, y2, 0.0]);
-    drawVector(v2, "blue");
+  // clear the canvas and set bg color to black
+  board.clearRect(0, 0, 400, 400);
+  board.fillStyle = 'black';
+  board.fillRect(0, 0, 400, 400);
+  // read the values of the text boxes to create v1 and v2.
+  let v1_x = document.getElementById('v1_x').value;
+  let v1_y = document.getElementById('v1_y').value;
+  let v2_x = document.getElementById('v2_x').value;
+  let v2_y = document.getElementById('v2_y').value;
+  let v1 = new Vector3([v1_x, v1_y, 0.0]);
+  let v2 = new Vector3([v2_x, v2_y, 0.0]);
+  // Call drawVector(v1, "red") and drawVector(v2, "blue")
+  drawVector(v1, "red");
+  drawVector(v2, "blue");
 }
 
 function handleDrawOperationEvent() {
-    let x = document.getElementById('v1x').value;
-    let y = document.getElementById('v1y').value;
-    let x2 = document.getElementById('v2x').value;
-    let y2 = document.getElementById('v2y').value;
-
-    // Clear Canvas
-    ctx.clearRect(0, 0, 400, 400);
-
-    // Black blackground
-    ctx.fillStyle = 'black'; // Set color to black
-    ctx.fillRect(0, 0, 400, 400);
-    let v1 = new Vector3([x, y, 0.0]);
-    drawVector(v1, "red");
-    let v2 = new Vector3([x2, y2, 0.0]);
-    drawVector(v2, "red");
-
-
-    
-    let operator = document.getElementById('operation_selection').value;
-    
-    switch (operator) {
-        case "Add":
-            v1.add(v2);
-            drawVector(v1,"green");
-            break;
-        case "Subtract":
-            v1.sub(v2);
-            drawVector(v1, "green");
-            break;
-        case "Multiply":
-            let scalarVal = document.getElementById('scalar').value;
-            v1.mul(scalarVal);
-            v2.mul(scalarVal);
-            drawVector(v1, "green");
-            drawVector(v2, "green");
-            break;
-        case "Divide":
-            let s = document.getElementById('scalar').value;
-            v1.div(s);
-            drawVector(v1, "green");
-            v2.div(s);
-            drawVector(v2, "green");
-            break;
-        case "Mag":
-            console.log("Magnitude v1: " + v1.magnitude());
-            console.log("Magnitude v2: " + v2.magnitude());
-            break;
-        case "Norm":
-            let v1_norm = v1.normalize();
-            drawVector(v1_norm, "green");
-            let v2_norm = v2.normalize();
-            drawVector(v2_norm, "green");
-            break;
-        case "Angle":
-            console.log("Angle: " + (angleBetween(v1, v2)));
-            break;
-        case "Area":
-            console.log("Area of this triangle: " + (areaTriangle(v1, v2)));
-            break;
-    }
+  let v1_x = document.getElementById('v1_x').value;
+  let v1_y = document.getElementById('v1_y').value;
+  let v2_x = document.getElementById('v2_x').value;
+  let v2_y = document.getElementById('v2_y').value;
+  board.clearRect(0, 0, 400, 400);
+  board.fillStyle = 'black';
+  board.fillRect(0, 0, 400, 400);
+  let v1 = new Vector3([v1_x, v1_y, 0.0]);
+  let v2 = new Vector3([v2_x, v2_y, 0.0]);
+  // Call drawVector(v1, "red") and drawVector(v2, "blue")
+  drawVector(v1, "red");
+  drawVector(v2, "blue");
+  let operation_val = document.getElementById('operation').value;
+  let scalarVal = document.getElementById('scalar').value;
+  switch (operation_val) {
+    case "add":
+        drawVector(v1.add(v2),"green");
+        break;
+    case "subtract":
+        drawVector(v1.sub(v2), "green");
+        break;
+    case "multiply":
+        drawVector(v1.mul(scalarVal), "green");
+        drawVector(v2.mul(scalarVal), "green");
+        break;
+    case "divide":
+        drawVector(v1.div(scalarVal), "green");
+        drawVector(v2.div(scalarVal), "green");
+        break;
+    case "magnitude":
+        console.log("Magnitude of v1: ", v1.magnitude());
+        console.log("Magnitude of v2: ", v2.magnitude());
+        break;
+    case "norm":
+        drawVector(v1.normalize(), "green");
+        drawVector(v2.normalize(), "green");
+        break;
+    case "angle":
+        console.log("Angle: ", (angleBetween(v1, v2)));
+        break;
+    case "area":
+        console.log("Area of Triangle: ", (areaTriangle(v1, v2)));
+        break;
+  }
 }
 
 function angleBetween(v1, v2) {
-    var m1 = v1.magnitude();
-    var m2 = v2.magnitude();
-    var d = Vector3.dot(v1, v2);
-    var alpha = Math.acos(d / (m1 * m2)); // Radians
-    alpha *= 180 / Math.PI;
-    return alpha;
+  var alpha = Math.acos(Vector3.dot(v1, v2) / (v1.magnitude() * v2.magnitude()));
+  alpha *= 180 / Math.PI;
+  return alpha;
 }
 
 function areaTriangle(v1, v2) {
-    var area = Vector3.cross(v1, v2);
-    var v1 = new Vector3([area[0], area[1], area[2]]);
-    var result = v1.magnitude() / 2;
-    return result;
+  var area = Vector3.cross(v1, v2);
+  var new_vector = new Vector3([area[0], area[1], area[2]]);
+  return new_vector.magnitude() / 2;
 }
