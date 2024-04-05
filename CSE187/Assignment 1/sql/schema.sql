7916412d353b74f5d2c804079e9f89c3916da657
Vince L <viwliu@ucsc.edu>
2024-04-05 14:19:44 -0700

    sql

--- CSE187/Assignment 1/sql/schema.sql
+++ CSE187/Assignment 1/sql/schema.sql
@@ -3,3 +3,7 @@ DROP TABLE IF EXISTS member CASCADE;
 CREATE TABLE member(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), data jsonb);
 
 -- Your schema DDL (create table statements etc.) goes below here 
+
+-- posts
+-- list of friends 
+--  friend requests
